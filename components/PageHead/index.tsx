9eeb5fb18b95cc6c6fd211e49a873e72e3a00ae5
Aleem Isiaka <aleemisiaka@gmail.com>
2021-08-01 13:19:58 +0100

    Multi screen support

--- components/PageHead/index.tsx
+++ components/PageHead/index.tsx
@@ -8,19 +8,7 @@ function PageHead() {
         <h2 className={styles.pageHeadTitle}>All Products</h2>
         <p className={styles.pageHeadSlug}>A 360° look with lumin</p>
       </div>
-      <div>
-        <select className={styles.pageHeadSelectInput}>
-          <option disabled>filter by:</option>
-          <option selected value="All Products">
-            All Products
-          </option>
-          <option value="New Products">New Products</option>
-          <option value="Packs">Packs</option>
-          <option value="Skin Care">Skin Care</option>
-          <option value="Hair and body care">Hair and body care</option>
-          <option value="Accessories">Accessories</option>
-        </select>
-      </div>
+      <div></div>
     </div>
   );
 }
